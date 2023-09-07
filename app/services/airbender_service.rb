f88d7ca65fbafa79b7fd729fcef2f99337d7a000
Allen Russell <garyrusselljr@gmail.com>
2023-09-07 12:38:50 -0600

    feat: setup airbender service with all available endpoints

---
+++ app/services/airbender_service.rb
@@ -0,0 +1,47 @@
+class AirbenderService
+
+  def all_characters
+    get_url('/api/v1/characters')
+  end
+
+  def single_character(character_id)
+    get_url("/api/v1/characters/#{character_id}")
+  end
+
+  def characters_with_nation_affiliation(affilation)
+    get_url("/api/v1/characters?affiliation=#{affiliation}")
+  end
+
+  def list_enemies_of(character)
+    get_url("/api/v1/characters?enemies=#{character}")
+  end
+
+  def list_allies_of(character)
+    get_url("/api/v1/characters?allies=#{character}")
+  end
+
+  def characters_that_match_partial(string)
+    get_url("/api/v1/characters?name=#{string}")
+  end
+
+  def one_random_character
+    get_url("/api/v1/characters/random")
+  end
+
+  def multiple_random_characters(count)
+    get_url("/api/v1/characters/random?count=#{count}")
+  end
+
+  def all_avatars
+    get_url("/api/v1/characters/avatar")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn 
+    Faraday.new(url: 'last-airbender-api.fly.dev')
+  end
+end
