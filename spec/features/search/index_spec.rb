dc55f78516fdcf1b485b84bad9dd282e5c855903
Allen Russell <garyrusselljr@gmail.com>
2023-09-08 10:30:23 -0600

    add details to search index view

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe "Search Index Page" do
       it "I should see the 'total number' of people who live in the 'Fire Nation'" do
         expect(page).to have_current_path(search_path)
         expect(page).to have_content(total number: 20)
+        save_and_open_page
       end
 
       it "I should see a list with detailed information for the first 25 members of the nation, and for each of the members I should see their name (and photo if available), list of allies or 'none', list of enemies or 'none', and any affiliations that member has" do
